5607387f55f7fc4acff97cb42b594be637304579
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-21 12:53:18 +0530

    [Viraj] Add.UC1 Hello from bridgelabz

---
+++ src/main/java/com/helloworld/HelloWorld/Controller.java
@@ -0,0 +1,12 @@
+package com.helloworld.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello From BridgeLab";
+    }
+}
